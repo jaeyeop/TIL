6873301e348d0b3697332c28928be167046daa73
jerry <fopu3316@naver.com>
2023-02-28 00:05:35 +0900

    2023-02-27

--- kotlin/kotlin-perfect-guide/src/main/kotlin/Chapter9/9_1_1.kt
+++ kotlin/kotlin-perfect-guide/src/main/kotlin/Chapter9/9_1_1.kt
@@ -2,46 +2,46 @@ package Chapter9
 
 class TypeParameter
 
-class TreeNode<T>(val data:T) {
-    private val _children = arrayListOf<TreeNode<T>>()
-    var parent: TreeNode<T>? = null
-        private set
-
-    val children: List<TreeNode<T>> get() = _children
-
-    fun addChild(data: T) = TreeNode(data).also {
-        _children += it
-        it.parent = this
-    }
-
-//    fun <T> TreeNode<T>.walkDepthFirst(action: (T) -> Unit) {
-//        children.forEach( it.walkDepthFirst(action) )
+//class TreeNode<T>(val data:T) {
+//    private val _children = arrayListOf<TreeNode<T>>()
+//    var parent: TreeNode<T>? = null
+//        private set
+//
+//    val children: List<TreeNode<T>> get() = _children
+//
+//    fun addChild(data: T) = TreeNode(data).also {
+//        _children += it
+//        it.parent = this
 //    }
-
-    override fun toString(): String =
-        _children.joinToString(prefix = "$data {", postfix = "}")
-
-}
-
-// Extension Function
-fun <T> TreeNode<T>.addChildren(vararg data: T) {
-    data.forEach { addChildren(it) }
-}
-
-// Extension Function
-val <T> TreeNode<T>.depth: Int
-    get() = (children.asSequence().map { it.depth }.maxOrNull() ?: 0) + 1
-
-fun main() {
-    val root = TreeNode("Hello").apply {
-        addChild("World")
-        addChild("!!!")
-    }
-
-    println(root)
-    println(root.children)
-    print(root.depth)
-}
+//
+////    fun <T> TreeNode<T>.walkDepthFirst(action: (T) -> Unit) {
+////        children.forEach( it.walkDepthFirst(action) )
+////    }
+//
+//    override fun toString(): String =
+//        _children.joinToString(prefix = "$data {", postfix = "}")
+//
+//}
+//
+//// Extension Function
+//fun <T> TreeNode<T>.addChildren(vararg data: T) {
+//    data.forEach { addChildren(it) }
+//}
+//
+//// Extension Function
+//val <T> TreeNode<T>.depth: Int
+//    get() = (children.asSequence().map { it.depth }.maxOrNull() ?: 0) + 1
+//
+//fun main() {
+//    val root = TreeNode("Hello").apply {
+//        addChild("World")
+//        addChild("!!!")
+//    }
+//
+//    println(root)
+//    println(root.children)
+//    print(root.depth)
+//}
 
 open class DataHolder<T>(val data: T)
 //
