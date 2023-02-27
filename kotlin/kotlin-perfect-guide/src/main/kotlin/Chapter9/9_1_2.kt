969676a4d19a153df75753c28b86e7d9593c4036
jerry <fopu3316@naver.com>
2023-02-28 00:46:13 +0900

    2023-02-27

--- kotlin/kotlin-perfect-guide/src/main/kotlin/Chapter9/9_1_2.kt
+++ kotlin/kotlin-perfect-guide/src/main/kotlin/Chapter9/9_1_2.kt
@@ -16,19 +16,55 @@ class TreeNode<T>(val data:T) {
         it.parent = this
     }
 
-    // Extension Function
-    fun <T> TreeNode<T>.addChildren(vararg data: T) {
-        data.forEach { addChildren(it) }
-    }
-
-//    이거 잘 모르겠는데..
-//    fun <T> TreeNode<T>.walkDepthFirst(action: (T) -> Unit) {
-//        children.forEach( it.walkDepthFirst(action) )
-//        action(data)
-//    }
-
     override fun toString(): String =
         _children.joinToString(prefix = "$data {", postfix = "}")
 
+}
+
+// Extension Function
+// https://velog.io/@kyy00n/kotlin-in-Action-Extension-Function-%ED%99%95%EC%9E%A5-%ED%95%A8%EC%88%98
+fun <T> TreeNode<T>.addChildren(vararg data: T) {
+    data.forEach { addChild(it) }
+}
+
+fun <T> TreeNode<T>.walkDepthFirst(action: (T) -> Unit) {
+    children.forEach{ it.walkDepthFirst(action) }
+    action(data)
+}
+
+fun <T : Number> TreeNode<T>.average(): Double {
+    var count = 0
+    var sum = 0.0
+    walkDepthFirst {
+        count++
+        sum += it.toDouble()
+    }
+    return sum/count
+}
+
+fun main() {
+    val intTree = TreeNode(1).apply {
+        addChildren(2,3,4,5)
+        println(average())
+    }
+    println(intTree.javaClass.name)
+
+    val intTree2 = TreeNode(1).apply {
+        addChild(2).addChild(3)
+        addChild(4)
+    }
+    println(intTree2)
+
+    val doubleTree = TreeNode(1.0).apply {
+        addChildren(2.0,3.0,)
+        println(average())
+    }
+    println(doubleTree.javaClass.name)
+
+    val stringTree = TreeNode("String").apply {
+        addChildren("a","b","c")
+//        println(average())
+    }.toString()
+    println(stringTree)
 }
 
